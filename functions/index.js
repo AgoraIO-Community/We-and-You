38841f36e4c285515107a816221ab5113c24ef45
Rohan Pandey <rohan@Rohans-MBP.hsd1.ca.comcast.net>
2018-08-07 05:38:51 -0700

    firebaseExpress

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,44 @@
 const functions = require('firebase-functions');
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//----------------------------ROUTES----------------------------
+app.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+app.get('/users', function (req, res, next) {
+    res.render('agora', { title: 'Agora Shk' });
+});
+//----------------------------ROUTES----------------------------
+
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+exports.application = functions.https.onRequest(app);
